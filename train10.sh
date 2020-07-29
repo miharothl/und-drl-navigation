6886ec04713411c3f03b249c3ec0e7591995a02f
Miha <miha@rothl.com>
2020-07-29 10:32:35 +0100

    gpu

--- train10.sh
+++ train10.sh
@@ -1,7 +1,7 @@
-nohup ./rlab -vvv -t -e breakup-nn01 2>&1 > train-1.log & \
-nohup ./rlab -vvv -t -e breakup-nn02 2>&1 > train-2.log & \
-nohup ./rlab -vvv -t -e breakup-nn03 2>&1 > train-3.log & \
-nohup ./rlab -vvv -t -e breakup-nn04 2>&1 > train-4.log & \
-nohup ./rlab -vvv -t -e breakup-nn05 2>&1 > train-5.log & \
-nohup ./rlab -vvv -t -e breakup-nn06 2>&1 > train-6.log & \
-nohup ./rlab -vvv -t -e breakup-nn07 2>&1 > train-7.log
+nohup ./rlab -vvv -t -e breakout-nn01 2>&1 > train-1.log & \
+nohup ./rlab -vvv -t -e breakout-nn02 2>&1 > train-2.log & \
+nohup ./rlab -vvv -t -e breakout-nn03 2>&1 > train-3.log & \
+nohup ./rlab -vvv -t -e breakout-nn04 2>&1 > train-4.log & \
+nohup ./rlab -vvv -t -e breakout-nn05 2>&1 > train-5.log & \
+nohup ./rlab -vvv -t -e breakout-nn06 2>&1 > train-6.log & \
+nohup ./rlab -vvv -t -e breakout-nn07 2>&1 > train-7.log
